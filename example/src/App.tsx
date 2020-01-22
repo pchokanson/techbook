350074627d3de0f55339eddee62c39f7e23aaadb
Peter Hokanson <pchokanson@gmail.com>
2020-01-22 00:27:54 -0600

    Update example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 
 import { LoremIpsum } from 'lorem-ipsum';
-import { Chapter } from './reactComponentLib';
+import { Book, Chapter, TableOfContents } from './reactComponentLib';
 
 class App extends Component {
   render() {
     const lorem = new LoremIpsum({});
     return (
       <div>
-        <Chapter title="Hello, world">
-          <p>{lorem.generateWords(32)}</p>
-          <p>{lorem.generateWords(32)}</p>
-        </Chapter>
+        <Book title="Hello, World!">
+          <TableOfContents />
+          <Chapter title={lorem.generateWords(4)}>
+            <p>{lorem.generateWords(32)}</p>
+            <p>{lorem.generateWords(32)}</p>
+          </Chapter>
+          <Chapter title={lorem.generateWords(4)}>
+            <p>{lorem.generateWords(32)}</p>
+            <p>{lorem.generateWords(32)}</p>
+          </Chapter>
+          <Chapter title={lorem.generateWords(4)}>
+            <p>{lorem.generateWords(32)}</p>
+            <p>{lorem.generateWords(32)}</p>
+          </Chapter>
+        </Book>
       </div>
     );
   }
